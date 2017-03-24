87a9a63292db93243dcca07ef5778fea0b013ca5
louise <kaelenmedeiros@gmail.com>
2017-03-24 00:50:37 +0000

    initial commit

--- language_detector/main.py
+++ language_detector/main.py
@@ -5,4 +5,31 @@
 def detect_language(text, languages):
     """Returns the detected language of given text."""
     # implement your solution here
-    pass
+    answer = text.split()
+    
+    english_count = 0
+    spanish_count = 0
+    german_count = 0
+
+    # adg psuedocode
+    for language in languages:
+        if language['name'] == 'Spanish':
+            for word in language['common_words']:
+                spanish_count += answer.count(word)
+                
+       
+        if language['name'] == 'German':
+            for word in language['common_words']:
+                german_count += answer.count(word)
+                
+        
+        if language['name'] == 'English':
+            for word in language['common_words']:
+                english_count += answer.count(word)
+                
+    if english_count > spanish_count and english_count > german_count:
+        return "English"
+    elif spanish_count > english_count and spanish_count > german_count:
+        return "Spanish"
+    elif german_count > english_count and spanish_count < german_count:
+        return "German"
